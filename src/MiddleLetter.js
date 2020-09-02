a7d6b14ae50df9d6250b3dad8d4a052e60da16d8
Tim <TimCPB@github.com>
2020-09-02 17:29:36 +0100

    green test 'returns 'es' when given 'test''

--- src/MiddleLetter.js
+++ src/MiddleLetter.js
@@ -1,20 +1,11 @@
 function getMiddle(word){
-  // var midValue = Math.ceil(word.length / 2)
-  // var array = word.split('')
-  // console.log(word)
-  // console.log(midValue)
-  // return word;
 
-  if(word.length > 2) {
+  if(word.length % 2 !== 0) {
     var midValue = Math.floor(word.length / 2)
     return word.charAt(midValue)
   }
   else
-    return word
-
-  // var length = word.length
-  // var array = word.split('')
-  // var midValue = (length / 2) rounded up
-
+    var midValue = word.length / 2
+    return word.charAt(midValue -1) + word.charAt(midValue)
 
 };
