1152d648c4ded95ee5940edb8981c1371f28f0ad
Tim <TimCPB@github.com>
2020-09-02 17:21:47 +0100

    green test 'returns 'a' when given 'cat''

--- src/MiddleLetter.js
+++ src/MiddleLetter.js
@@ -1,3 +1,20 @@
 function getMiddle(word){
-  return word;
+  // var midValue = Math.ceil(word.length / 2)
+  // var array = word.split('')
+  // console.log(word)
+  // console.log(midValue)
+  // return word;
+
+  if(word.length > 2) {
+    var midValue = Math.floor(word.length / 2)
+    return word.charAt(midValue)
+  }
+  else
+    return word
+
+  // var length = word.length
+  // var array = word.split('')
+  // var midValue = (length / 2) rounded up
+
+
 };
