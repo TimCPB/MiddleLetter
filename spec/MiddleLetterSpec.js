1c52be2b646629a08bff1489a301fa30f01cd53f
Tim <TimCPB@github.com>
2020-09-02 12:40:34 +0100

    red test 'returns 'a' when given 'cat''

--- spec/MiddleLetterSpec.js
+++ spec/MiddleLetterSpec.js
@@ -7,4 +7,8 @@ describe("getMiddle", function() {
   it("returns 'Of' when given 'Of'", function() {
     expect(getMiddle("Of")).toEqual("Of")
   })
-})
+
+  it("returns 'a' when given 'cat'", function() {
+    expect(getMiddle("cat")).toEqual("a")
+  })
+});
