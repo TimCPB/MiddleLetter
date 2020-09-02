4ca01dbd521a3b9fa71e3a982fa8d91f7a96bb78
Tim <TimCPB@github.com>
2020-09-02 17:46:08 +0100

    refactor with getMidValue function

--- src/MiddleLetter.js
+++ src/MiddleLetter.js
@@ -1,15 +1,19 @@
 function getMiddle(word){
 
   if(_isOdd(word)) {
-    var midValue = Math.floor(word.length / 2)
-    return word.charAt(midValue)
+    // var midValue = Math.floor(word.length / 2)
+    return word.charAt(getMidValue(word))
   }
   else
-    var midValue = word.length / 2
-    return word.charAt(midValue -1) + word.charAt(midValue)
+    // var midValue = word.length / 2
+    return word.charAt(getMidValue(word) - 1) + word.charAt(getMidValue(word))
 
 };
 
 function _isOdd(word){
   return word.length % 2 !== 0
+}
+
+function getMidValue(word){
+  return Math.floor(word.length/2)
 }
