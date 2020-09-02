3c325ff91dc90d2becb649ead248537e129ca9b9
Tim <TimCPB@github.com>
2020-09-02 12:37:56 +0100

    red test 'returns 'Of' when given 'Of''

--- spec/MiddleLetterSpec.js
+++ spec/MiddleLetterSpec.js
@@ -3,4 +3,8 @@ describe("getMiddle", function() {
   it("returns 'A' when given 'A'", function() {
     expect(getMiddle("A")).toEqual("A")
   })
+
+  it("returns 'Of' when given 'Of'", function() {
+    expect(getMiddle("Of")).toEqual("Of")
+  })
 })
